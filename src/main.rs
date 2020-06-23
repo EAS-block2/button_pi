6105b937b9d1b20d00375cb57cc9420c73d9d4e3
Jacob Ellington <jake@workstation.ellingtech.com>
2020-06-23 12:34:41 -0400

    add return

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ fn main() -> std::io::Result<()> {
     let mut stream = TcpStream::connect("192.168.1.144:5432")?;
 
     stream.write(&[1])?;
-    stream.read(&mut [0; 128])?;
+    let a = stream.read(&mut [0; 128])?;
+    println!("{:?}", a);
     Ok(())
 }
