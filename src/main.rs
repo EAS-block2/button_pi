0a77a524331952061647d06397c48351577c68b1
Jacob Ellington <jake@workstation.ellingtech.com>
2020-06-24 11:41:07 -0400

    testing

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,9 @@ use std::net::TcpStream;
 fn main() -> std::io::Result<()> {
     let mut stream = TcpStream::connect("192.168.1.144:5432")?;
 
-    stream.write(&[1])?;
-    let a = stream.read(&mut [0; 128])?;
+    stream.write(b"Hello World")?;
+    /*let a = stream.read(&mut [0; 128])?;
     println!("{:?}", a);
-    Ok(())
+    Ok(())*/
+    println!("Done!")
 }
