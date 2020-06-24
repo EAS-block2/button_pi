aea469f8d3ed0dc79cd3f97a77a66183d72e0bf0
Jacob Ellington <jake@workstation.ellingtech.com>
2020-06-24 11:44:29 -0400

    fixes

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 use std::io::prelude::*;
 use std::net::TcpStream;
 
-fn main() -> std::io::Result<()> {
-    let mut stream = TcpStream::connect("192.168.1.144:5432")?;
+fn main(){
+    match alert(){
+        Ok(_) => {
+            println!("success! exiting.");
+        }
+        Err(_) => {println!("failed.");}
+    }
+}
 
+fn alert() -> std::io::Result<()> {
+    let mut stream = TcpStream::connect("192.168.1.144:5432")?;
     stream.write(b"Hello World")?;
     /*let a = stream.read(&mut [0; 128])?;
-    println!("{:?}", a);
-    Ok(())*/
-    println!("Done!")
+    println!("{:?}", a);*/
+    Ok(())
 }
