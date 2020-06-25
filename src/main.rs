e10122235135761da24bb7de3612c589d25f511e
Jacob Ellington <jake@workstation.ellingtech.com>
2020-06-25 12:54:28 -0400

    add reply test

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::io::prelude::*;
 use std::net::TcpStream;
-
+use std::str;
 fn main(){
     match alert(){
         Ok(_) => {
@@ -13,7 +13,16 @@ fn main(){
 fn alert() -> std::io::Result<()> {
     let mut stream = TcpStream::connect("192.168.1.144:5432")?;
     stream.write(b"Hello World")?;
-    /*let a = stream.read(&mut [0; 128])?;
-    println!("{:?}", a);*/
+    let mut data = [0 as u8; 50];
+    match stream.read(&mut data){
+        Ok(size) => {
+           match str::from_utf8(&data[0..size]){
+               Ok(string_out) => {
+                   println!("Got data: {}", string_out);}
+               Err(_) => {println!("fault");}
+           }
+        }
+        Err(_) => {println!("Fault when reading data!");}
+    }
     Ok(())
 }
