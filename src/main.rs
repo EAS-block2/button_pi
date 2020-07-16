d42e1b43c9e5623262bd0b5e97f09ce345c6a25e
Jacob Ellington <jake@workstation.ellingtech.com>
2020-07-16 13:45:56 -0400

    yeet

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,27 @@ use std::net::TcpStream;
 use std::process::Command;
 use dns_lookup;
 use std::{thread, time, str};
-use gpio_cdev::{Chip, LineRequestFlags, EventRequestFlags, EventType};
+use gpio_cdev::{Chip, LineRequestFlags};
 fn main(){
     let mut chip = Chip::new("/dev/gpiochip0").unwrap();
-    let button = chip.get_line(15).unwrap();
+    let button = chip.get_line(15).unwrap().request(LineRequestFlags::INPUT, 1, "Button Switch pin").unwrap();
     let mut pressed = false;
-
-spawn_button().unwrap();
-loop{
-    thread::sleep(time::Duration::from_secs(3));
-}
-}
+    loop{
+    thread::sleep(time::Duration::from_millis(100));
+    match button.get_value().unwrap(){
+        1 => {pressed = false;},
+        0 => { //a button press actually pulls the pin low
+            if !pressed{
+            pressed = true;
+            match alert(){
+                Ok(_) => {
+                    println!("success!");
+                    success_flash();
+                }
+                Err(e) => {println!("ERROR: {}",e);
+            on_failure();}
+    }}}
+    _ => {println!("Got other input");}}}}
 
 fn alert() -> std::io::Result<()> {
     let hostname: String;
@@ -35,22 +45,19 @@ fn alert() -> std::io::Result<()> {
         Err(_) => {println!("Fault when reading data!");}}
     Ok(())
 }
-/*fn success_flash(){
+fn success_flash(){
     let mut value = false;
-    let mut light: gpio::sysfs::SysFsGpioOutput;
-    match gpio::sysfs::SysFsGpioOutput::open(21){
-        Ok(e)=> {light = e;}
-        Err(_)=> return,
-    }
     let mut counter = 0;
+    let mut chip = Chip::new("/dev/gpiochip0").unwrap();
+    let light = chip.get_line(21).unwrap().request(LineRequestFlags::OUTPUT, 1, "Button Light pin").unwrap();
     thread::spawn(move || loop {
         counter +=1;
-        match light.set_value(value){ Ok(_)=>(), Err(_) => break}
+        match light.set_value(value as u8){ Ok(_)=>(), Err(_) => break}
         thread::sleep(time::Duration::from_millis(250));
         value = !value;
         if counter > 480 {break;}
 });
-}*/
+}
 fn on_failure(){
     let mut counter = 0;
     loop{
@@ -58,26 +65,9 @@ fn on_failure(){
         counter +=1;
     match alert(){
         Ok(_) => {println!("finally got connection.");
-            //success_flash();
+            success_flash();
             break;}
         Err(_) => {
             if counter > 10{Command::new("reboot");}
             else{thread::sleep(time::Duration::from_secs(30));}
         }}}}
-fn spawn_button()-> gpio_cdev::errors::Result<()> {
-    let mut chip = Chip::new("/dev/gpiochip0")?;
-    let input = chip.get_line(15)?;
-    thread::spawn(move || loop {
-        for event in input.events(LineRequestFlags::INPUT,EventRequestFlags::BOTH_EDGES,"Button Listen Thread").unwrap(){
-            let evt = event.unwrap();
-            match evt.event_type() {
-                EventType::RisingEdge => {
-                    println!("rising edge");
-                }
-                EventType::FallingEdge => {
-                    println!("rising edge");
-                }
-        }
-}});
-Ok(())
-}
