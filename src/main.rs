bdc0a0cb158e1268d1df1b189f859a6a32f750b0
Jacob Ellington <jake@workstation.ellingtech.com>
2020-07-17 12:48:59 -0400

    testing silent behavior

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ fn alert() -> std::io::Result<()> {
         Ok(hostn) => {hostname = hostn;}
         Err(_) => {hostname = "unknown".to_string();}
     }
-    let mut stream = TcpStream::connect("192.168.1.162:5432")?;
+    let mut stream = TcpStream::connect("192.168.1.162:5433")?;
     let to_send = hostname.into_bytes();
     stream.write(to_send.as_slice())?;
     let mut data = [0 as u8; 50];
