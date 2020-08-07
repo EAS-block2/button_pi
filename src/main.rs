2345cc93728b09951619504b17bd91b8f872200d
Jacob Ellington <jake@chi.ellingtech.com>
2020-08-07 17:48:45 -0400

    remote work

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::io::prelude::*;
 use std::net::TcpStream;
-use std::process::Command;
 use dns_lookup;
 use serde_yaml;
 use serde::Deserialize;
@@ -11,31 +10,37 @@ fn main(){
     init();
     loop{
         println!("heartbeat");
-        thread::sleep(time::Duration::from_secs(15));
+        thread::sleep(time::Duration::from_secs(25));
     }
 }
-fn init(){
+fn init() {
     let conf_f = std::fs::File::open("/home/pi/config.yaml").expect("can't find config");
     let config: Config = serde_yaml::from_reader(conf_f).expect("Bad YAML config file!");
     let mut chip = Chip::new("/dev/gpiochip0").unwrap();
-    let g_in = chip.get_line(15).unwrap().request(LineRequestFlags::INPUT, 1, "Button Switch pin").unwrap();
-    let g_out = 21;
-    let s_in = chip.get_line(23).unwrap().request(LineRequestFlags::INPUT, 1, "Button Switch pin").unwrap();
-    let s_out = 12;
-    let mut gen_addr: String = config.server_addr;
-    let mut sil_addr = gen_addr.clone();
-    gen_addr.push_str(&config.general_port);
-    sil_addr.push_str(&config.silent_port);
-    let mut gen_alm = Alarm{address: gen_addr, input: g_in, output: g_out, pressed: false};
-    let mut sil_alm = Alarm{address: sil_addr, input: s_in, output: s_out, pressed: false};
-    thread::spawn(move || loop{
-        gen_alm.run();
-        thread::sleep(time::Duration::from_millis(100))});
-    thread::spawn(move || loop{
-        sil_alm.run();
-        thread::sleep(time::Duration::from_millis(100))});
- 
-}
+    match chip.get_line(15).unwrap().request(LineRequestFlags::INPUT, 1, "Button Switch pin"){
+        Ok(g_in) => {
+            let g_out = 21;
+            let mut gen_addr: String = (&config.server_addr).to_string();
+            gen_addr.push_str(&config.general_port);
+            let mut gen_alm = Alarm{address: gen_addr, input: g_in, output: g_out, pressed: false};
+            thread::spawn(move || loop{
+                gen_alm.run();
+                thread::sleep(time::Duration::from_millis(100))});
+        }
+        Err(_) => {println!("General Alm pin already registered, skipping.")}
+    }
+    match chip.get_line(23).unwrap().request(LineRequestFlags::INPUT, 1, "Button Switch pin"){
+    Ok(s_in) => {
+        let s_out = 12;
+        let mut sil_addr: String = (&config.server_addr).to_string();
+        sil_addr.push_str(&config.silent_port);
+        let mut sil_alm = Alarm{address: sil_addr, input: s_in, output: s_out, pressed: false};
+        thread::spawn(move || loop{
+            sil_alm.run();
+            thread::sleep(time::Duration::from_millis(100))});
+    }
+    Err(_) => {println!("Silent Alm pin already registered, skipping.")}
+}}
 
 #[derive(Deserialize)]
 struct Config{
@@ -51,6 +56,7 @@ struct Alarm{
 }
 impl Alarm{
     fn run(&mut self){
+        println!("{} Sent Heartbeat", self.address);
         match self.input.get_value().unwrap(){
             1 => {self.pressed = false;},
             0 => { //a button press pulls the pin low
@@ -95,7 +101,7 @@ impl Alarm{
             match self.success_flash(){Ok(_)=>(),Err(_)=>{println!("already flashing");}}
                 break;}
             Err(_) => {
-                if counter > 10{Command::new("reboot");}
+                if counter > 10{panic!("cannot connect to server");}
                 else{thread::sleep(time::Duration::from_secs(30));}
             }}}}
     fn success_flash(&self) -> gpio_cdev::errors::Result<()>{
